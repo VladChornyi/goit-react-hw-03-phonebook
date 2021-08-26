3b1e8d0431f42af4bedfafcce73563e6386e0540
Vlad <4ornii.vlad@gmail.com>
2021-08-26 16:44:50 +0300

    добавлена возможность сохранения контактов в localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,54 +5,56 @@ import Filter from './filter/Filter';
 import shortid from 'shortid';
 
 class App extends Component {
- state = {
-   contacts: [
-     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-   ],
-   filter:''
- 
- }
-  addContact = (number) => {
-    const searchRepeat = this.state.contacts
-      .map((user) => user.name.toLowerCase())
-      .includes(number.name.toLowerCase());
-    
-    if (searchRepeat) {
-      alert(`${number.name} is already in contacts`)
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({ contacts: savedContacts });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
-    else {
+  }
+
+  addContact = number => {
+    const searchRepeat = this.state.contacts
+      .map(user => user.name.toLowerCase())
+      .includes(number.name.toLowerCase());
+
+    if (searchRepeat) {
+      alert(`${number.name} is already in contacts`);
+    } else {
       const contact = {
         ...number,
-        id: shortid.generate()
-      }
+        id: shortid.generate(),
+      };
       this.setState(prev => ({ contacts: [...prev.contacts, contact] }));
     }
   };
 
-
   getFilteredContacts = () => {
-
     const normalizedFilter = this.state.filter.toLowerCase();
 
     return this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter))
-  }
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
 
-   changeFilter = (filter) => {
+  changeFilter = filter => {
     this.setState({ filter });
-   };
-  
-   removeContact = (contactId) => {
-    this.setState((prevState) => {
+  };
+
+  removeContact = contactId => {
+    this.setState(prevState => {
       return {
         contacts: prevState.contacts.filter(({ id }) => id !== contactId),
       };
     });
   };
-  
+
   render() {
     const { filter } = this.state;
     return (
@@ -63,8 +65,11 @@ class App extends Component {
 
           <h2>Contacts</h2>
           <Filter value={filter} onChangeFilter={this.changeFilter} />
-          <ContactsList contacts={this.getFilteredContacts()} onRemoveContact={this.removeContact}  />
-      </div>
+          <ContactsList
+            contacts={this.getFilteredContacts()}
+            onRemoveContact={this.removeContact}
+          />
+        </div>
       </>
     );
   }
